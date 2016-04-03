5066be029afca9641a8d331908ea6115381ec325
Herbert Kagumba <herbert.kagumba@andela.com>
2016-04-03 20:17:06 +0300

    test (static_pages): updated the static_pages testing and added Guard

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Learning rails"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Learning rails"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Learning rails"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Learning rails"
+    assert_select "title", "About Me | #{@base_title}"
   end
 
 end
